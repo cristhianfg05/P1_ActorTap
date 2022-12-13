1bfa1a5d62eb72d41348b6618199ad2155ca770e
nagu10 <nahuelsw99@hotmail.com>
2022-12-13 16:24:55 +0100

    Revert "Repartacioones"
    This reverts commit 87f779c064aadddfe4eeb91a88e38b4af058925e.

--- P1-Actor/src/RingActor.java
+++ P1-Actor/src/RingActor.java
@@ -24,14 +24,10 @@ public class RingActor implements ActorInterface, Runnable{
     @Override
     public void send(MessageInterface message) {
         /**FALTA CORREGIR QUE CADA RING SOLO ENVIE MENSAJE AL SIGUIENTE RING ACTOR**/
-        if(this.nextActor != null){
-            message.setSender(this);
-            message.setReciever(this.nextActor);
-            this.nextActor.getQueueMsg().add(message);
-            this.nextActor.send(message);
-        }else
-            System.out.println("NO HAY MAS ACTORES ENCADENADOS");
-
+        ActorInterface aux = message.getReciever();
+        message.setSender(this);
+        message.setReciever(aux);
+        aux.getQueueMsg().add(message);
     }
 
     @Override
@@ -43,10 +39,8 @@ public class RingActor implements ActorInterface, Runnable{
     public void run() {
         while(true){
             try {
-                MessageInterface msg = this.queueMsg.take();
-                if(!(msg instanceof QuitMessage))
-                    process(this.queueMsg.take());
-                else break;
+                System.out.println("LLego el ring run");
+                process(this.queueMsg.take());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
